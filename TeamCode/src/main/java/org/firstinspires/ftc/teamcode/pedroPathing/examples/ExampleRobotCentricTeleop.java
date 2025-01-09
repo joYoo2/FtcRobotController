b2290b78581ba79fcf40b279d7be58a4653b1d74
Dylan <dylandw2009@gmail.com>
2025-01-08 22:05:06 -0500

    experimentation pedro teleop (NOT FUNCTIONING 100%)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/examples/ExampleRobotCentricTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/examples/ExampleRobotCentricTeleop.java
@@ -4,6 +4,10 @@ import android.provider.SyncStateContract;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
 import com.pedropathing.util.Constants;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -19,11 +23,36 @@ import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
  * @version 2.0, 12/30/2024
  */
 
-//@Disabled
-//@TeleOp(name = "Example Robot-Centric Teleop", group = "Examples")
+@TeleOp(name = "Example Robot-Centric Teleop", group = "Examples")
 public class ExampleRobotCentricTeleop extends OpMode {
     private Follower follower;
     private final Pose startPose = new Pose(0,0,0);
+
+    private final Pose grabBackPose = new Pose(20, 33, Math.toRadians(0));
+    private final Pose grabPose = new Pose(10, 33, Math.toRadians(0));
+
+    private final Pose scorePose = new Pose(39, 60, Math.toRadians(180));
+
+
+    private PathChain grabSpecimen, scoreSpecimen;
+
+    public void buildPaths(){
+        grabSpecimen = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(follower.getPose()), new Point(grabBackPose)))
+                .setLinearHeadingInterpolation(follower.getPose().getHeading(), grabPose.getHeading(), 0.5)
+                .addPath(new BezierLine(new Point(grabBackPose), new Point(grabPose)))
+                .setLinearHeadingInterpolation(grabBackPose.getHeading(), grabPose.getHeading())
+                .setZeroPowerAccelerationMultiplier(1)
+                .build();
+
+        //this isnt rly gonna work tho bc is just gonna place at the same spot all the time but whatever og
+        scoreSpecimen = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(grabPose), new Point(scorePose)))
+                .setLinearHeadingInterpolation(grabPose.getHeading(), scorePose.getHeading(), 0.5)
+                .setPathEndTimeoutConstraint(200)
+                .setPathEndHeadingConstraint(.007)
+                .build();
+    }
 
     /** This method is call once when init is played, it initializes the follower **/
     @Override
@@ -31,6 +60,7 @@ public class ExampleRobotCentricTeleop extends OpMode {
         Constants.setConstants(FConstants.class,LConstants.class);
         follower = new Follower(hardwareMap);
         follower.setStartingPose(startPose);
+        buildPaths();
     }
 
     /** This method is called continuously after Init while waiting to be started. **/
@@ -57,6 +87,12 @@ public class ExampleRobotCentricTeleop extends OpMode {
 
         follower.setTeleOpMovementVectors(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x, true);
         follower.update();
+
+        if(gamepad2.touchpad){
+            follower.followPath(grabSpecimen);
+        }else if(gamepad2.share){
+            follower.followPath(scoreSpecimen);
+        }
 
         /* Telemetry Outputs of our Follower */
         telemetry.addData("X", follower.getPose().getX());
